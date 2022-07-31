a44127b02cb8cce7af47173f1c716a669a8c85b0
Basil Reji <basilrejidev@gmail.com>
2022-07-31 17:47:37 +0530

    new sections added

--- javascripts/script.js
+++ javascripts/script.js
@@ -146,6 +146,33 @@
         }
     });
 
+    new Swiper('.testimonials-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    });
+
+
     // Animation on scroll
     window.addEventListener('load', () => {
         AOS.init({
@@ -157,5 +184,7 @@
     });
 
     new PureCounter();
+
+
 
 })()
